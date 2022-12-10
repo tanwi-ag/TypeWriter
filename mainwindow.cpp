d656fee128b176f4c10ae4ae08dd019b7253e9b0
Tanwi Agrawal <tanwiagrawal09@gmail.com>
2022-12-10 14:55:32 +0545

    Initial Commit

---
+++ mainwindow.cpp
@@ -0,0 +1,157 @@
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+
+
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_actionNew_triggered()
+{
+
+    currentFile.clear();
+
+    ui->textEdit->setText(QString());
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Open the file");
+
+    QFile file(fileName);
+    currentFile = fileName;
+
+    if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+
+    QTextStream in(&file);
+    QString text = in.readAll();
+
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+void MainWindow::on_actionSave_as_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "Save as");
+
+    QFile file(fileName);
+
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+
+    currentFile = fileName;
+
+    setWindowTitle(fileName);
+
+    QTextStream out(&file);
+
+    QString text = ui->textEdit->toPlainText();
+
+    out << text;
+
+    file.close();
+}
+
+void MainWindow::on_actionSave_triggered()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "Save");
+    QFile file(fileName);
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+    currentFile = fileName;
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+void MainWindow::on_actionExit_triggered()
+{
+    QApplication::quit();
+}
+
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
+
+void MainWindow::on_actionAbout_triggered()
+{
+    QString url = "https://drive.google.com/file/d/1_FUZfe7Pm89VZ4yoeKbsJbeyJn2Za2Bp/view?usp=share_link";
+    QDesktopServices::openUrl(url);
+}
+
+void MainWindow::on_actionBold_2_triggered()
+{
+    QFont font = ui -> textEdit -> currentFont();
+        font.bold() == true ? font.setBold(false) : font.setBold(true);
+        ui -> textEdit -> setCurrentFont(font);
+}
+
+
+
+void MainWindow::on_actionSubScript_triggered(bool checked)
+{
+    QTextCharFormat format;
+    if(checked == true)
+        format.setVerticalAlignment(QTextCharFormat::AlignSubScript);
+    else
+        format.setVerticalAlignment(QTextCharFormat::AlignNormal);
+
+    ui -> textEdit -> mergeCurrentCharFormat(format);
+}
+
+
+void MainWindow::on_actionSuperScript_triggered(bool checked)
+{
+    QTextCharFormat format;
+    if(checked == true)
+        format.setVerticalAlignment(QTextCharFormat::AlignSuperScript);
+    else
+        format.setVerticalAlignment(QTextCharFormat::AlignNormal);
+
+    ui -> textEdit -> mergeCurrentCharFormat(format);
+}
+
+
---
+++ mainwindow.h
@@ -0,0 +1,71 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QPrinter>
+#include <QPrintDialog>
+#include <QFont>
+#include <QMainWindow>
+#include <QtCore>
+#include <QTextCharFormat>
+#include <QtWidgets>
+
+// Use the standard UI namespace which is tied to the .ui file
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_as_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
+
+    void on_actionAbout_triggered();
+
+    void on_actionBold_2_triggered();
+
+    void on_actionSubScript_triggered(bool checked);
+
+    void on_actionSuperScript_triggered(bool checked);
+
+private:
+
+
+    Ui::MainWindow *ui;
+
+    QString currentFile = "";
+};
+
+#endif // MainWindow_H
+
+
